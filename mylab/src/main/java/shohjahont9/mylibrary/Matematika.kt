9464a9cc653948b83e0f63275b465d313a7848dc
Shohjahont9 <Shohjahont9@gmail.com>
2020-09-18 15:20:32 +0500

    helloWorld

---
+++ mylab/src/main/java/shohjahont9/mylibrary/Matematika.kt
@@ -0,0 +1,10 @@
+package shohjahont9.mylibrary
+
+class Matematika {
+    companion object {
+        fun qoshish(num1: Int, num2: Int) = num1 + num2
+        fun ayirish(num1: Int, num2: Int) = num1 -  num2
+        fun kopaytiruv(num1: Int, num2: Int) = num1 * num2
+        fun bolish(num1: Int, num2: Int) = num1 / num2
+    }
+}
